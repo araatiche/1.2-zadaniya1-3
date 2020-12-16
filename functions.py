92ff1b878ba37710e9486ee86671988838dd0e4b
araatiche <araatiche@yandex.ru>
2020-12-16 17:12:42 +0300

    funcions modified

--- functions.py
+++ functions.py
@@ -6,3 +6,10 @@ for i in range(0,list_length):
     user_defined_list.append(n) # заполняем список случайными числами от 1 до 50 в цикле, количество итераций задано пользователем
 print("Полученный список:")
 print(user_defined_list)
+def avg_value(list):
+    list_elements_sum = 0
+    for e in list:
+        list_elements_sum += e
+    average = list_elements_sum / len(list)
+    return average
+print("Среднее значение: ", avg_value(user_defined_list))
