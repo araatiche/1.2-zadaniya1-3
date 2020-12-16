1b69e8dee294a2316812719eea1e1173e4c3cde6
araatiche <araatiche@yandex.ru>
2020-12-16 21:17:30 +0300

    3 task done!

--- files.py
+++ files.py
@@ -1,2 +1,13 @@
+score_sum = 0 # инициализируем переменные для суммы оценок и количества строк в файле
+line_count = 0
+print("Оценка меньше 3 у следующих учеников:")
+# без явного указания кодировки файл из задания не открывается (UnicodeDecodeError: 'charmap' codec can't decode byte 0x98 in position 1: character maps to <undefined>)
 f = open('result.txt',"r",encoding="utf-8")
-print(f.read())
+for line in f:
+    current_line = line.rstrip() # функция rstrip() удаляет символ перевода строки из строки
+    score_sum += int(current_line[-1]) # чтобы можно было взять последний элемент строки (оценка) как -1й элемент
+    if int(current_line[-1]) < 3: # в случае если оценка меньше 3 выводим данную строку на экран 
+        print(current_line)
+    line_count += 1   # счетчик строк для подсчета среднго балла
+awg_score = score_sum / line_count # считаем средний балл, выводим на экран
+print("Средний бал: ",str(awg_score))
--- functions.py
+++ functions.py
@@ -7,22 +7,22 @@ for i in range(0,list_length):
 print("Полученный список:")
 print(user_defined_list)
 # функция вычисляющая среднее значение чисел в списке
-def avg_value(list):
-    list_elements_sum = sum(list) # встроенная функция sum вычисляет сумму элементов списка
-    average = list_elements_sum / len(list) 
+def avg_value(input_list):
+    list_elements_sum = sum(input_list) # встроенная функция sum вычисляет сумму элементов списка
+    average = list_elements_sum / len(input_list) 
     return average 
 # функция вычисляющая cумму с помощью цикла for
-def for_summ(list):
+def for_summ(input_list):
     summ_f = 0
-    for sf in list: # sf каждый цикл принимает текущее значение элемента list от первого до последнего 
+    for sf in input_list: # sf каждый цикл принимает текущее значение элемента input_list от первого до последнего 
         summ_f += sf # суммируем их в summ_f
     return summ_f
 # функция вычисляющая cумму с помощью цикла while
-def while_summ(list):
+def while_summ(input_list):
     summ_w = 0 
     sw = 0
-    while (sw < len(list)): # пока sw меньше длинны входного списка
-        summ_w += list[sw] # суммируем в summ_w значение элемента списка с индексом, равным текущему значению sw
+    while (sw < len(input_list)): # пока sw меньше длинны входного списка
+        summ_w += input_list[sw] # суммируем в summ_w значение элемента списка с индексом, равным текущему значению sw
         sw += 1 # инкримируем sw
     return summ_w
 # выводим результаты всех функций
